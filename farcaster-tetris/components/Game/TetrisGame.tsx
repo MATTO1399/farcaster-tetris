d31f2c51ce7a7197fc6a806783754701a8803832
MATTO1399 <monoomoraeru243@icloud.com>
2026-06-17 13:50:34 +0900

    fix: use score variable for MintNFT

--- farcaster-tetris/components/Game/TetrisGame.tsx
+++ farcaster-tetris/components/Game/TetrisGame.tsx
@@ -258,7 +258,7 @@ const TetrisGame: React.FC<TetrisGameProps> = ({ onGameOver }) => {
                   
                   {/* NFTミントボタン */}
                   <div className="mb-4 relative z-[110]">
-                    {currentUserAddress && <MintNFT score={lastScore} />}
+                    {currentUserAddress && <MintNFT score={score} />}
                   </div>
 
                   <div className="flex flex-col gap-3 w-full">
